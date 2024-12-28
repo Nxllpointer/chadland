4a9995ddd570bfe4fd4f8f0149bbbbda5100ea84
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-12-28 12:17:20 +0100

    Add logging env filter

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    tracing_subscriber::fmt().init();
+    if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
+        tracing_subscriber::fmt().with_env_filter(env_filter).init();
+    } else {
+        tracing_subscriber::fmt().init();
+    }
 
     chadland::run::<chadland::backends::winit::WinitBackend>();
 }
